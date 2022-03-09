3e8574c03d748de7e16f9182ca2fd8fe0cf26eca
mincheol <homerjay@naver.com>
2022-03-10 06:51:40 +0900

    Initial Commit

---
+++ swift_NetflixUITests/swift_NetflixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_NetflixUITestsLaunchTests.swift
+//  swift_NetflixUITests
+//
+//  Created by 申民鐡 on 2022/03/10.
+//
+
+import XCTest
+
+class swift_NetflixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
