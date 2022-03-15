ae1e4acb74b4db11322b3905ba65fc1ea8ea2ade
mincheol <homerjay@naver.com>
2022-03-15 11:47:21 +0900

    tableviewHeader 옵션

--- swift_Netflix/Controllers/HomeViewController.swift
+++ swift_Netflix/Controllers/HomeViewController.swift
@@ -69,6 +69,17 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController:UITableViewDelegate, UITableViewDataSource {
     
+    //tableview의 헤더에 섹션에 관한 옵션을 설정할 수 있다.
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        //header에 대입 - view가 있으면 UITableViewheaderFotterview로 리턴해주고 아니면 아무것도 아닌것을 리턴.
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20,
+                                         y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .label
+        //header의 텍스트를 row스펠링으로 바꿔줄 수 있다.
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+    }
     
     //tableview에서 title을 표현해주는 기본메서드
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
